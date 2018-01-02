26f0304e4b209fbaae63548e147ae9d0ff0f8138
Kevin Hannigan <kph1958@rit.edu>
2018-01-01 19:40:26 -0500

    Setting up an express server to serve our content

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
+const path = require('path');
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT  || 3000;
+const port = process.env.PORT || 3000;
+const env = process.env.ENV || 'dev';
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  stats: {
-    colors: true,
-  },
-})
-  .listen(port, 'localhost', err => {
-    if (err)console.log(err);
-    console.log('Listening at ' + host + ':' + port);
-  });
+const express = require('express');
+let app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Webpack hot reload middleware
+if(env === 'dev') {
+  const webpack = require('webpack');
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const config = require('./webpack.config');
+  const compiler = webpack(config);
+
+  app.use(webpackMiddleware(compiler, {
+    // webpack-dev-middleware options
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+      colors: true,
+    },
+  }));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => console.log("Listening on port", port));
