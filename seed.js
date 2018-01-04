010502787422352d7221420eceec4f9a814a71b5
Kevin Hannigan <kph1958@rit.edu>
2018-01-04 16:02:51 -0500

    Use winston for logging

--- seed.js
+++ seed.js
@@ -1,6 +1,9 @@
+const logger = require('./logger');
 if(!process.env.ENV) {
-  throw 'Seeding is a destructive action.'+
-        'You *must* specify the environment using `ENV=dev|test|prod`';
+  logger.error(
+    'Seeding is a destructive action.'+
+    'You *must* specify the environment using `ENV=dev|test|prod`');
+  process.exit(1);
 }
 
 const config = require('./config/config');
@@ -10,7 +13,7 @@ mongoose.connect(config.db.address);
 const Plan = require('./model/plan.js');
 const SEDefault = require('./seed-data/SE.js');
 
-console.log('Removing all plans');
+logger.info('Removing all plans');
 Plan.remove({})
   .then(() => {
     return Plan.create(SEDefault);
@@ -22,6 +25,6 @@ Plan.remove({})
     process.exit(0);
   })
   .catch((error) => {
-    console.log(error);
+    logger.error(error);
     process.exit(1);
   });
